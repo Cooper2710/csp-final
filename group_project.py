b5ce21a53104da12cd141ad2b6031115db5b0bb0
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-10-24 05:52:33 +0000

    2345678uioppoi87654321345678i9oiuhjytr54r5ey6jugmhgfbddrrtyi45678iuhgtr4dfghjjhgfdfghjkjhgfdfghjklkjhgfdfghjkjhgfdsdfghjkjhgfdsdfghjkjhgfdsdfghjk

--- group_project.py
+++ group_project.py
@@ -1,29 +1,108 @@
-#hang man by Emma and Cooper and Nate 
+#dollar store oregon trail by Emma and Cooper and Nate 
 
-# Make the hangman-cooper
-#________
-#|      |
-#|      0
-#|     /|\  
-#|     / \
-#|_________
-#make function that draws the hangman-Emma
-def draw (hangman) 
-
-#set the word and what is correct and line unerneth-Nate Phillip Castro 
-import random
-    word = ["Hello", ""]
+#text that says you are in the dollar store oregon trail, you have a wife and a kid, and a wagon hauled by an ox with little supplies. Your goal is to get from minnesota to oregon.
 
 
-#Function and variable that deals with the user input "what letter" -Nate
+#You have to pass through five states, there is a challenge in each state
 
 
-#if letter is not correct, then add a body part to the hangman-Emma
-
-#loop that repeat until either word is correct, and you win, or you lose because your bad -Cooper
-def ___
+#create a user input function that asks your name, your wifes name, your kids name, and the ox's name
 
 
+#create a function that states you died because you were stupid. This will be called later whenever you make a wrong descicion
 
 
+#create loop in a function that counts how many current correct answers you have out of 5 to be called in the terminal
 
+
+#if statement that asks if you want to travel over the mountains or through the plains
+
+
+#IF YOU CHOSE OVER THE MOUNTAINS, THIS SECTION OF CODE WILL START
+
+
+#First challenge is in north dakota, your family is starving, will you hunt or eat your supplies question 1/5, your in minnesota
+
+
+#if you chose to eat your supplies you didnt have enough and you died
+
+
+#if you chose too go hunt, are you hunting deer or a bear question 2/5 your in north dakota
+
+
+#if you chose bear, you get eaten and die
+
+
+#if you chose deer, you live and feed your family, but it wasnt a whole lot
+
+
+#your in Montana, humgry again, go fishing or use supplies question 3/5
+
+
+#if you use supplies, not enough and ya die
+
+
+#if you fish, you get a lucky massive trout, and feed your family
+
+
+#you arrive in idaho, set up camp in the cold winter, or stay in someones cabin question 4/5
+
+
+#if you decide to sleep in cabin, you sleep in thomas creech's cabin. you get murdered
+
+
+#if you set up camp, its cold, but your family endures.
+
+
+#you get to the final stretch in oregon, do you set up camp again or push through
+
+
+#if you set up camp, you get bit by a rattlesnake and die in your sleep
+
+
+#if you push through, you make is and win with your family
+
+
+#IF YOU DECIDED TO GO THROUGH THE PLAINS, THIS SECTION ACTIVATES
+
+
+#your in minnesota, your family is starting to get hungry. do you hunt or use supplies? question 1/5
+
+
+#if you use supplies, you get dysentery and die for no apparent reason
+
+
+#If you chose to hunt, you can either hunt bison or a bunch of prarie dogs in south dakota question 2/5
+
+
+#if you hunt bison, you get trampled and die
+
+
+#if you hunt prarie dogs, you get a bunch and they last you and your family
+
+
+#you arrive in wyoming, unbeknownst to you the prarie dogs had Tuberculosis, and your whole family is infected. Thug it out or ge a witch doctor to to a magic dance around your fire
+
+
+#if you thug it out, you die
+
+
+#if you chose the witch doctor, your family is completely cured. 
+
+
+#you arrive in idaho, your kid wants to take a break and play. do you be a good father and play tag or shrug him off
+
+
+#if you play with little jimmy, he sprains his ankle and dies.
+
+
+#if you shrug him off, you avoid catrastrophe and move on. 
+
+
+#your in oregon, the final stretch, your family is impatient. do you pick up the pace or play it safe
+
+
+#if you pick up the pace, you trip, break your arm, sever your carotid artery, and die
+
+
+#if you play it safe, your family makes it safe and sound and you win
