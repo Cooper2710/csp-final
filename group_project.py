1fabc6440ec9025595be38c844657f4ec64e661e
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-10-24 02:29:07 +0000

    dfgh

---
+++ group_project.py
@@ -0,0 +1,29 @@
+#hang man by Emma and Cooper and Nate 
+
+# Make the hangman-cooper
+#________
+#|      |
+#|      0
+#|     /|\  
+#|     / \
+#|_________
+#make function that draws the hangman-Emma
+def draw (hangman) 
+
+#set the word and what is correct and line unerneth-Nate Phillip Castro 
+import random
+    word = ["Hello", ""]
+
+
+#Function and variable that deals with the user input "what letter" -Nate
+
+
+#if letter is not correct, then add a body part to the hangman-Emma
+
+#loop that repeat until either word is correct, and you win, or you lose because your bad -Cooper
+def ___
+
+
+
+
+
