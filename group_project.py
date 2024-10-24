721da2227a3993730f09773cef98b9ced9dbd2fc
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-10-24 17:07:03 +0000

    the fitnessgram pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues

--- group_project.py
+++ group_project.py
@@ -1,108 +1,111 @@
 #dollar store oregon trail by Emma and Cooper and Nate 
 
-#text that says you are in the dollar store oregon trail, you have a wife and a kid, and a wagon hauled by an ox with little supplies. Your goal is to get from minnesota to oregon.
+#text that says you are in the dollar store oregon trail, you have a wife and a kid, and a wagon hauled by an ox with little supplies. Your goal is to get from minnesota to oregon. - cooper
 
 
-#You have to pass through five states, there is a challenge in each state
+#You have to pass through five states, there is a challenge in each state - emma
 
 
-#create a user input function that asks your name, your wifes name, your kids name, and the ox's name
+#create a user input function that asks your name, your wifes name, your kids name, and the ox's name - nate
 
 
-#create a function that states you died because you were stupid. This will be called later whenever you make a wrong descicion
+#create a function that states you died because you were stupid. This will be called later whenever you make a wrong descicion- cooper
 
 
-#create loop in a function that counts how many current correct answers you have out of 5 to be called in the terminal
+#create loop in a function that counts how many current correct answers you have out of 5 to be called in the terminal - emma
 
 
-#if statement that asks if you want to travel over the mountains or through the plains
+#if statement that asks if you want to travel over the mountains or through the plains - nate
 
 
 #IF YOU CHOSE OVER THE MOUNTAINS, THIS SECTION OF CODE WILL START
 
 
-#First challenge is in north dakota, your family is starving, will you hunt or eat your supplies question 1/5, your in minnesota
+#First challenge is in north dakota, your family is starving, will you hunt or eat your supplies question 1/5, your in minnesota - cooper
 
 
-#if you chose to eat your supplies you didnt have enough and you died
+#if you chose to eat your supplies you didnt have enough and you died - emma
 
 
-#if you chose too go hunt, are you hunting deer or a bear question 2/5 your in north dakota
+#if you chose too go hunt, are you hunting deer or a bear question 2/5 your in north dakota - nate
 
 
-#if you chose bear, you get eaten and die
+#if you chose bear, you get eaten and die - cooper
 
 
-#if you chose deer, you live and feed your family, but it wasnt a whole lot
+#if you chose deer, you live and feed your family, but it wasnt a whole lot - emma 
 
 
-#your in Montana, humgry again, go fishing or use supplies question 3/5
+#your in Montana, humgry again, go fishing or use supplies question 3/5 - nate
 
 
-#if you use supplies, not enough and ya die
+#if you use supplies, not enough and ya die - cooper
 
 
-#if you fish, you get a lucky massive trout, and feed your family
+#if you fish, you get a lucky massive trout, and feed your family - emma
 
 
-#you arrive in idaho, set up camp in the cold winter, or stay in someones cabin question 4/5
+#you arrive in idaho, set up camp in the cold winter, or stay in someones cabin question 4/5 - nate
 
 
-#if you decide to sleep in cabin, you sleep in thomas creech's cabin. you get murdered
+#if you decide to sleep in cabin, you sleep in a man named thomas creech's cabin. you get murdered - cooper
 
 
-#if you set up camp, its cold, but your family endures.
+#if you set up camp, its cold, but your family endures. - emma
 
 
-#you get to the final stretch in oregon, do you set up camp again or push through
+#you get to the final stretch in oregon, do you set up camp again or push through - nate
 
 
-#if you set up camp, you get bit by a rattlesnake and die in your sleep
+#if you set up camp, you get bit by a rattlesnake and die in your sleep - cooper
 
 
-#if you push through, you make is and win with your family
+#if you push through, you make is and win with your family - emma
 
 
-#IF YOU DECIDED TO GO THROUGH THE PLAINS, THIS SECTION ACTIVATES
+#IF YOU DECIDED TO GO THROUGH THE PLAINS, THIS SECTION ACTIVATES - nate
 
 
-#your in minnesota, your family is starting to get hungry. do you hunt or use supplies? question 1/5
+#your in minnesota, your family is starting to get hungry. do you hunt or use supplies? question 1/5 - cooper
 
 
-#if you use supplies, you get dysentery and die for no apparent reason
+#if you use supplies, you get dysentery and die for no apparent reason - emma 
 
 
-#If you chose to hunt, you can either hunt bison or a bunch of prarie dogs in south dakota question 2/5
+#If you chose to hunt, you can either hunt bison or a bunch of prarie dogs in south dakota question 2/5 - nate
 
 
-#if you hunt bison, you get trampled and die
+#if you hunt bison, you get trampled and die - cooper
 
 
-#if you hunt prarie dogs, you get a bunch and they last you and your family
+#if you hunt prarie dogs, you get a bunch and they last you and your family - emma 
 
 
-#you arrive in wyoming, unbeknownst to you the prarie dogs had Tuberculosis, and your whole family is infected. Thug it out or ge a witch doctor to to a magic dance around your fire
+#you arrive in wyoming, unbeknownst to you the prarie dogs had Tuberculosis, and your whole family is infected. Thug it out or ge a witch doctor to to a magic dance around your fire - nate
 
 
-#if you thug it out, you die
+#if you thug it out, you die - cooper
 
 
-#if you chose the witch doctor, your family is completely cured. 
+#if you chose the witch doctor, your family is completely cured. - emma
 
 
-#you arrive in idaho, your kid wants to take a break and play. do you be a good father and play tag or shrug him off
+#you arrive in idaho, your kid wants to take a break and play. do you be a good father and play tag or shrug him off - nate
 
 
-#if you play with little jimmy, he sprains his ankle and dies.
+#if you play with little jimmy, he sprains his ankle and dies. - cooper
 
 
-#if you shrug him off, you avoid catrastrophe and move on. 
+#if you shrug him off, you avoid catrastrophe and move on. - emma 
 
 
-#your in oregon, the final stretch, your family is impatient. do you pick up the pace or play it safe
+#your in oregon, the final stretch, your family is impatient. do you pick up the pace or play it safe - nate
 
 
-#if you pick up the pace, you trip, break your arm, sever your carotid artery, and die
+#if you pick up the pace, you trip, break your arm, sever your carotid artery, and die - cooper
 
 
-#if you play it safe, your family makes it safe and sound and you win
+#if you play it safe, your family makes it safe and sound and you win - emma 
+
+
+#print statement that tells you to to refresh to replay - nate
